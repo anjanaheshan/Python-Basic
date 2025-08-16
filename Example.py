46800f657d9939bf3659f17818caaa77c5efda33
Anjana Heshan <anjanaheshan289@gmail.com>
2025-08-16 17:22:44 +0530

    Multification Table

--- Example.py
+++ Example.py
@@ -1,10 +1,6 @@
-n1=int(input("Enter n1: "))
-n2=int(input("Enter n2: "))
-n3=int(input("Enter n3: "))
-if n1 > n2 and n1 > n3:
-   max= n1
-elif n2 > n1 and n2 > n3:
-    max= n2
-else:
-    max= n3
-    print("The maximum number:", max)
+n=int(input("Enter an Integer Number: "))
+i=1
+while(i<=100):
+    print(i,"*",n,"=" , (i*n))
+    i += 1
+
